90c2d90d790097e9d928ba26766274b425ee124a
AlexSarsam <AlexSarsam@users.noreply.github.com>
2026-06-02 02:26:43 +0200

    arreglar estructura archivos

--- ActividadBoostrap/javascript.js
+++ ActividadBoostrap/javascript.js
@@ -1,1 +1,10 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+window.addEventListener('scroll', function () {
+  var secciones = document.querySelectorAll('.reveal');
+
+  secciones.forEach(function (seccion) {
+    var posicion = seccion.getBoundingClientRect().top;
+    if (posicion < window.innerHeight - 100) {
+      seccion.classList.add('visible');
+    }
+  });
+});
